bd79d2e1c23cfaeed0182a515966e9a2db040a7e
MaxiBracamonte <maxi.b88@gmail.com>
2022-05-19 19:07:18 -0300

    header+logo+redes+banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { LogoAPComponent } from './componentes/logo-ap/logo-ap.component';
+import { RedesComponent } from './componentes/redes/redes.component';
+import { BannerComponent } from './componentes/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    RedesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
